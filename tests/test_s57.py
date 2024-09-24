27debd2201f61f4ff5774f602a6281ac0c18b245
Patryk Korczak <patkor9@gmail.com>
2024-09-24 15:12:30 +0200

    Added GDB file configuration

--- tests/test_s57.py
+++ tests/test_s57.py
@@ -1,15 +1,6 @@
-import math
 from seacharts.enc import ENC
-from seacharts.core.extent import Extent
-from pyproj import Transformer
+
 
 enc = ENC("config.yaml")
-# ships = [
-#         (1, 612291, 6071542, 132, "orange"),
-#         (2, 356393, 6052365, 57, "yellow")
-#     ]
-
-# enc.display.add_vessels(*ships)
-# enc.display.start()
-
+enc.display.start()
 enc.display.show()
